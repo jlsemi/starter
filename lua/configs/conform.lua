a322dee7a77f22d8ddeaffcc3fffc2197e656648
johnc <jialer.2007@gmail.com>
2025-05-08 10:00:13 +0800

    feat: support golang dev

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    go = { "gofmt", "goimports" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+
+  formatters = {
+    gofmt = {
+      prepend_args = { "-s" },
+    },
+    goimports = {
+      prepend_args = { "-local", "" },
+    },
+  }
 }
 
 return options
